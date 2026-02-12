88c0bd0402225f3a0e36b93492c7bdfd36160901
ZhangBigball <2496165769@qq.com>
2026-02-12 16:56:31 +0800

    训练代码-初始化参数

--- LeNet/train.py
+++ LeNet/train.py
@@ -1,10 +1,13 @@
+import torch
+import torch.utils.data as data
+import torch.nn as nn
 from torchvision.datasets import FashionMNIST
 from torchvision import transforms
-import torch.utils.data as data
 import numpy as np
 import matplotlib.pyplot as plt
 from model import LeNet
-
+import copy
+import time
 
 # 数据预处理，划分训练集和验证集，加载数据
 def data_process():
@@ -27,3 +30,30 @@ def data_process():
                                       shuffle=False, 
                                       num_workers=8)
     return train_data_loader, val_data_loader
+
+
+# 训练函数
+def train(model, train_data_loader, val_data_loader, num_epochs):
+    # 选择设备并导入模型
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model = model.to(device)
+    # 损失函数
+    criterion = torch.nn.CrossEntropyLoss()
+    # 优化器
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+    # 最优模型
+    best_model = copy.deepcopy(model.state_dict())
+    # 最高准确度
+    best_acc = 0.0
+    # 训练集损失列表
+    train_loss_list = []
+    # 验证集损失列表
+    val_loss_list = []
+    # 训练集准确度列表
+    train_acc_list = []
+    # 验证集准确度列表
+    val_acc_list = []
+    # 当前时间
+    since = time.time()
+
+
