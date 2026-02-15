3fd06308c3617a6ae84c7e2d2b13ab93e72163cf
ZhangBigball <2496165769@qq.com>
2026-02-15 15:50:08 +0800

    推理代码

---
+++ VGG16/inference.py
@@ -0,0 +1,52 @@
+import torch
+import torch.utils.data as data
+from torchvision.datasets import FashionMNIST
+from torchvision import transforms
+from model import VGG16
+import random
+
+num2label = {
+    0: 'T-shirt/top',
+    1: 'Trouser',
+    2: 'Pullover',
+    3: 'Dress',
+    4: 'Coat', 
+    5: 'Sandal',
+    6: 'Shirt',
+    7: 'Sneaker',
+    8: 'Bag',
+    9: 'Ankle boot'
+    }
+
+# 生成随机测试样本的函数
+def generate_random_sample():
+    test_dataset = FashionMNIST(
+        root='./VGG16/Dataset',
+        train=False,
+        transform=transforms.Compose([transforms.Resize(size=224),transforms.ToTensor()]),
+        download=True)
+    
+    random_index = random.randint(0, len(test_dataset) - 1)
+    test_x, test_y = test_dataset[random_index]
+    test_x = test_x.unsqueeze(0)  # 增加batch维度
+    return test_x, test_y
+
+# 模型推理函数
+def inference(model, test_x):
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model = model.to(device)
+    model.eval()
+    with torch.no_grad():
+        test_x = test_x.to(device)
+        output = model(test_x)
+        pred_lab = torch.argmax(output, dim=1)
+        pred_lab = pred_lab.cpu().item()
+    return pred_lab
+
+if __name__ == "__main__":
+    model = VGG16()
+    model.load_state_dict(torch.load('./VGG16/vgg16.pth'))
+    for i in range(10):
+        test_x, test_y = generate_random_sample()
+        pred_lab = inference(model, test_x)
+        print(f"真实标签: {num2label[test_y]}, 预测标签: {num2label[pred_lab]}")
